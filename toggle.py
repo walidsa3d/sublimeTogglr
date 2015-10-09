7cf0deaf3df7fca2c2727a00b58fa8a7274e4cf7
walid <walid.sa3d@gmail.com>
2015-10-10 00:03:19 +0100

    add checkboxes

--- toggle.py
+++ toggle.py
@@ -33,3 +33,17 @@ class LnToggleDisplayCommand(sublime_plugin.TextCommand):
             propertyValue = propertyValue1 if settings.get(
                 propertyName, propertyValue1) != propertyValue1 else propertyValue2
             settings.set(propertyName, propertyValue)
+
+    def is_checked(self, **kwargs):
+        action = kwargs['action'].upper()
+        if action == "WHITE_SPACE":
+            sett = self.view.settings().get("draw_white_space")
+            if sett == "all":
+                return True
+            return False
+        if action == "LINE_NO":
+            return self.view.settings().get("line_numbers")
+        if action == "GUTTER":
+            return self.view.settings().get("gutter")
+        if action == "INDENT_GUIDE":
+            return self.view.settings().get("draw_indent_guides")
